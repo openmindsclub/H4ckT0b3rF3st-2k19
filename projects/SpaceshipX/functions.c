fb16a161a4ac9c3e83081b4b9df0a91c03a56c8f
Merzoug Imad-eddine <imadom568@gmail.com>
2019-10-28 13:01:04 +0100

    implement droks and game over menu
    thnx shell

--- projects/SpaceshipX/functions.c
+++ projects/SpaceshipX/functions.c
@@ -7,59 +7,83 @@
 #define WINDOW_HEIGHT 600
 
 
-void KeyboardInput(SDL_Event event, int *up, int *down, int *right, int *left,int *playing)
+void KeyboardInput(SDL_Event event, int *up, int *down, int *right, int *left,int *starting, int *playing, int *gameover_screen, int *closing, int *try_again)
 {
-	switch (event.type)
+	if(*starting)
 	{
-	case SDL_QUIT :
-		*playing = 0; break;
-	case SDL_KEYDOWN :
-		switch (event.key.keysym.scancode)
+		switch (event.type)
 		{
-		case SDL_SCANCODE_RIGHT:
-			*right = 1; break;
-		case SDL_SCANCODE_LEFT:
-			*left = 1; break;
-		case SDL_SCANCODE_UP:
-			*up = 1; break;
-		case SDL_SCANCODE_DOWN:
-			*down = 1; break;
-		case SDL_SCANCODE_ESCAPE:
-			*playing = 0; break;
-		default:
-			break;
-		}
-		break;
-	case SDL_KEYUP :
-		switch (event.key.keysym.scancode)
-		{
-		case SDL_SCANCODE_RIGHT:
-			*right = 0; break;
-		case SDL_SCANCODE_LEFT:
-			*left = 0; break;
-		case SDL_SCANCODE_UP:
-			*up = 0; break;
-		case SDL_SCANCODE_DOWN:
-			*down = 0; break;
-		default:
+		case SDL_QUIT :
+			*starting = *playing = *gameover_screen = 0; *closing = 1; break;
+		case SDL_KEYDOWN :
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_ESCAPE:
+				*starting = *playing = *gameover_screen = 0; *closing = 1;  break;
+			default:
+				break;
+			}
 			break;
 		}
 	}
-}
-
-
-
-void DisplayPlayer(int *flame, SDL_Renderer *renderer, SDL_Texture *tex_player, SDL_Rect *player_src, SDL_Rect *player_dst)
-{
-	int i = 0;
-
-	//animate the rocket's fire
-	if (*flame >= 30) *flame = 0;
-	i = (*flame/10);
-
-	(*flame)++;
-
-	SDL_RenderCopy(renderer, tex_player, &player_src[i], player_dst);
+	else if(*playing)
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT :
+			*starting = *playing = *gameover_screen = 0; *closing = 1; break;
+		case SDL_KEYDOWN :
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_RIGHT:
+				*right = 1; break;
+			case SDL_SCANCODE_LEFT:
+				*left = 1; break;
+			case SDL_SCANCODE_UP:
+				*up = 1; break;
+			case SDL_SCANCODE_DOWN:
+				*down = 1; break;
+			case SDL_SCANCODE_ESCAPE:
+				*starting = *playing = *gameover_screen = 0; *closing = 1; break;
+			default:
+				break;
+			}
+			break;
+		case SDL_KEYUP :
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_RIGHT:
+				*right = 0; break;
+			case SDL_SCANCODE_LEFT:
+				*left = 0; break;
+			case SDL_SCANCODE_UP:
+				*up = 0; break;
+			case SDL_SCANCODE_DOWN:
+				*down = 0; break;
+			default:
+				break;
+			}
+		}
+	}
+	else if(*gameover_screen)
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT :
+			*starting = *playing = *gameover_screen = 0; *closing = 1; break;
+		case SDL_KEYDOWN :
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_RETURN:
+				*try_again = 1; break;
+			case SDL_SCANCODE_ESCAPE:
+				*starting = *playing = *gameover_screen = 0; *closing = 1; break;
+			default:
+				break;
+			}
+			break;
+		}
+	}
 }
 
 
@@ -107,4 +131,140 @@ void UpdatePlayerPosition(SDL_Rect *player_dst, int up, int down, int right, int
 	if (player_dst->y < 0) player_dst->y = 0;
 	if (player_dst->x > WINDOW_WIDTH - player_dst->w) player_dst->x = WINDOW_WIDTH - player_dst->w;
 	if (player_dst->y > WINDOW_HEIGHT - player_dst->h) player_dst->y = WINDOW_HEIGHT - player_dst->h;
+}
+
+
+void DisplayPlayer(int *flame, SDL_Renderer *renderer, SDL_Texture *tex_player, SDL_Texture *tex_bg, SDL_Rect *player_src, SDL_Rect *player_dst, int *starting, int *playing, int *gameover_screen, int *closing)
+{
+	int i = 0;
+	if(*starting)
+	{
+		SDL_Event event;
+		while( *starting && (player_dst->y > WINDOW_HEIGHT * 3/4) )
+		{
+			while (SDL_PollEvent(&event))
+			{
+				KeyboardInput(event, NULL, NULL, NULL, NULL, starting, playing, gameover_screen, closing, NULL);
+			}
+
+			//animate the rocket's fire
+			if (*flame >= 30) *flame = 0;
+			i = (*flame/10);
+			(*flame)++;
+
+			SDL_RenderClear(renderer);
+			SDL_RenderCopy(renderer, tex_bg, NULL, NULL);
+			SDL_RenderCopy(renderer, tex_player, &player_src[i], player_dst);
+			SDL_RenderPresent(renderer);
+
+			player_dst->y -= 3;
+			SDL_Delay(1000/60);
+		}
+	}
+	else if(*playing)
+	{
+		//animate the rocket's fire
+		if (*flame >= 30) *flame = 0;
+		i = (*flame/10);
+		(*flame)++;
+
+		SDL_RenderCopy(renderer, tex_player, &player_src[i], player_dst);
+	}
+}
+
+
+void UpdateRocksPositions(int rock_num, int *speed, SDL_Rect *rock_src, SDL_Rect *rock_dst, int starting, int playing, int gameover_screen)
+{
+	int x = 0;
+        //initializing the rocks' positions 
+	if(starting)
+	{
+		for(int i=0; i<rock_num; i++)
+		{
+			x = (rand() % 4) + 6;
+			speed[i] = 10 - x;
+			rock_dst[i].w = rock_src->w * x/10;
+			rock_dst[i].h = rock_src->h * x/10;
+			rock_dst[i].x = rock_src->w * i;
+			rock_dst[i].y = - rock_dst[i].h - rand() % WINDOW_HEIGHT;
+		}
+	}
+	else if(playing)
+	{
+		for(int i=0; i<rock_num; i++)
+		{
+			//this respawn a rock after it goes off screen
+			if( rock_dst[i].y > WINDOW_HEIGHT )
+			{
+				x = (rand() % 4) + 6;
+				speed[i] = 10 - x;
+				rock_dst[i].w = rock_src->w * x/10;
+				rock_dst[i].h = rock_src->h * x/10;
+				rock_dst[i].y = - rock_dst[i].h - rand() % WINDOW_HEIGHT;
+			}
+			else
+			{
+				rock_dst[i].y += speed[i];
+			}
+		}
+	}
+	else if(gameover_screen)
+	{
+		for(int i=0; i<rock_num; i++)
+		{
+			//only move down a rock if it is already on the screen
+			if( (rock_dst[i].y < WINDOW_HEIGHT) && (rock_dst[i].y + rock_dst[i].h > 0) )
+			{
+				rock_dst[i].y += speed[i];
+			}
+		}
+	}
+}
+
+
+void DisplayRocks(SDL_Renderer *renderer, SDL_Texture *tex_rocks, SDL_Rect *rock_src, SDL_Rect *rock_dst, int rock_num, int *no_rock_left, int playing, int gameover_screen)
+{
+	if (playing)
+	{
+		for(int i=0; i<rock_num; i++)
+		{
+			SDL_RenderCopy(renderer, tex_rocks, rock_src, &rock_dst[i]);
+		}
+	}
+	else if (gameover_screen)
+	{
+		for (int i=0; i<rock_num; i++)
+		{
+			if( (rock_dst[i].y < WINDOW_HEIGHT) && (rock_dst[i].y + rock_dst[i].h > 0) )
+			{
+				SDL_RenderCopy(renderer, tex_rocks, rock_src, &rock_dst[i]);
+				*no_rock_left = 0;
+			}
+		}
+	}
+}
+
+
+
+int CheckIfLost(SDL_Rect player_dst, int rock_num, SDL_Rect *rock_dst)
+{
+	int tolerence = 10;
+	for (int i=0; i<rock_num; i++)
+	{
+		//if the player and the rock are in the same vertical line
+		if ( ( player_dst.x + player_dst.w - tolerence ) > rock_dst[i].x &&
+		     ( player_dst.x < (rock_dst[i].x + rock_dst[i].w - tolerence ) ) )
+		{
+			//if the player and the rock are in the same horizontale line
+			if ( (player_dst.y < (rock_dst[i].y + rock_dst[i].h - tolerence)) && 
+			     (player_dst.y + player_dst.h - tolerence) > rock_dst[i].y )
+			{
+				//the player has lost
+				return 1;
+			}
+		}
+	}
+
+	//the player did not lose yet
+	return 0;
 }
--- projects/SpaceshipX/game.c
+++ projects/SpaceshipX/game.c
@@ -1,9 +1,10 @@
-//gcc game.c functions.c -lSDL2 -lSDL2_ttf -lSDL2_image -Wall -o SpaceshipX
+//gcc game.c functions.c -lSDL2 -lSDL2_ttf -lSDL2_image
 
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 #include "functions.h"
@@ -15,15 +16,17 @@
 int main(int argc, char *argv[])
 {
 	srand(time(NULL));
-	int playing = 0, flame = 0;
+	int starting = 0, playing = 0, gameover_screen = 0, closing = 0, try_again = 0;
+	int flame = 0, rock_num = 0, no_rock_left = 0;
 	int up = 0, down = 0, left = 0, right = 0;
 
 	SDL_Window *window;
 	SDL_Renderer *renderer;
 	SDL_Surface *surface;
-	SDL_Texture *tex_bg, *tex_player;
-	SDL_Rect player_src[3], player_dst, player_dimentions;
+	SDL_Texture *tex_bg, *tex_player, *tex_rocks, *tex_gameover;
+	SDL_Rect player_src[3], player_dst, rock_src, player_dimentions, gameover_dst;
 	SDL_Event event;
+
 
 	//Initializing SDL
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
@@ -33,6 +36,17 @@ int main(int argc, char *argv[])
 				SDL_WINDOWPOS_CENTERED,
 				WINDOW_WIDTH, WINDOW_HEIGHT, 0);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+
+	//Initializing TTF
+	TTF_Init();
+	TTF_Font *font = TTF_OpenFont("font/benethFREE.ttf", 90);
+	SDL_Color White = {255, 255, 255};
+	if (font == NULL)
+	{
+		puts("Error : font file not found\n");
+		return 1; //return error
+	}
 	
 
 	//Loading the background image and creating a texture
@@ -56,6 +70,29 @@ int main(int argc, char *argv[])
 		return 1; //return error
 	}
 
+	//Loading the rocks image and creating a texture
+	surface = IMG_Load("images/rock.png");
+	tex_rocks = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_QueryTexture(tex_rocks, NULL, NULL, &rock_src.w, &rock_src.h);
+	SDL_FreeSurface(surface);
+        if (surface == NULL)
+        {
+		puts("Error : images/rock.png not found\n");
+                return 1; //return error
+        }
+
+	//Loading the game over image and creating a texture
+	surface = TTF_RenderText_Blended_Wrapped(font, "GAME OVER\n   press enter", White, 400);
+	tex_gameover = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
+	gameover_dst.w = 300;
+	gameover_dst.h= 150;
+
+	//initializing the rocks
+	rock_num = WINDOW_WIDTH / rock_src.w;
+	int speed[rock_num];
+	SDL_Rect rock_dst[rock_num]; 
+
 	//initializing the source rockets
 	for (int i=0; i<3; i++)
 	{
@@ -68,34 +105,88 @@ int main(int argc, char *argv[])
 	//initializing the destination rocket
 	player_dst.w = player_dimentions.w/3;
 	player_dst.h = player_dimentions.h;
-	player_dst.x = WINDOW_WIDTH/2 - player_dst.w/2;
-	player_dst.y = WINDOW_HEIGHT * 3/4;
 
-	playing = 1;
+	starting = 1;
 	//the game's main loop
-	while (playing) 
+	while (!closing) 
 	{
-		while (SDL_PollEvent(&event))
+		if (starting)//display the first animation
 		{
-			KeyboardInput(event, &up, &down, &right, &left, &playing);
+			up = down = left = right = 0;
+
+			player_dst.x = (WINDOW_WIDTH - player_dst.w) /2;
+			player_dst.y = WINDOW_HEIGHT;
+
+			DisplayPlayer(&flame, renderer, tex_player, tex_bg, player_src, &player_dst, &starting, &playing, &gameover_screen, &closing);
+			UpdateRocksPositions(rock_num, speed, &rock_src, rock_dst, starting, playing, gameover_screen);
+
+			starting = 0;
+			playing = !closing;
 		}
 
-		UpdatePlayerPosition(&player_dst, up, down, right, left);
+		while (playing)
+		{
+			//if the player hit a rock break from playing loop and enter gameover loop
+			gameover_screen = CheckIfLost(player_dst, rock_num, rock_dst);
+			playing = !gameover_screen;
+	
+			while (SDL_PollEvent(&event))
+			{
+				KeyboardInput(event, &up, &down, &right, &left, &starting, &playing, &gameover_screen, &closing, NULL);
+			}
 
-		SDL_RenderClear(renderer);
+			UpdatePlayerPosition(&player_dst, up, down, right, left);
+			UpdateRocksPositions(rock_num, speed, &rock_src, rock_dst, starting, playing, gameover_screen);
 
-		SDL_RenderCopy(renderer, tex_bg, NULL, NULL);	
+			SDL_RenderClear(renderer);
+			SDL_RenderCopy(renderer, tex_bg, NULL, NULL);	
+			
+			DisplayRocks(renderer, tex_rocks, &rock_src, rock_dst, rock_num, &no_rock_left, playing, gameover_screen);
+			DisplayPlayer(&flame, renderer, tex_player, tex_bg, player_src, &player_dst, &starting, &playing, &gameover_screen, &closing);
+
+			SDL_RenderPresent(renderer);
+			SDL_Delay(1000/60);
+		}
+
+		//initialize game over text position
+		gameover_dst.x = (WINDOW_WIDTH-gameover_dst.w) /2;
+		gameover_dst.y = (WINDOW_HEIGHT-gameover_dst.h) /2;
 		
-		DisplayPlayer(&flame, renderer, tex_player, player_src, &player_dst);
+		while (gameover_screen)
+		{
+			while (SDL_PollEvent(&event))
+			{
+				KeyboardInput(event,  NULL, NULL, NULL, NULL, &starting, &playing, &gameover_screen, &closing, &try_again);
+			}
 
-		SDL_RenderPresent(renderer);
-		SDL_Delay(1000/60);
+			no_rock_left = 1;//assume that there is no rock left in the screen
+			if (try_again) gameover_dst.y += 5;//if the player hit enter to play again the gameover text moves down 
+
+			SDL_RenderClear(renderer);
+                        SDL_RenderCopy(renderer, tex_bg, NULL, NULL);
+
+			UpdateRocksPositions(rock_num, speed, &rock_src, rock_dst, starting, playing, gameover_screen);
+			DisplayRocks(renderer, tex_rocks, &rock_src, rock_dst, rock_num, &no_rock_left, playing, gameover_screen);
+			
+			SDL_RenderCopy(renderer, tex_gameover, NULL, &gameover_dst);
+			SDL_RenderPresent(renderer);
+			SDL_Delay(1000/60);
+
+			//if the gameover text is no longer in the screen than break out of the loop and start a new game
+			if ( (gameover_dst.y > WINDOW_HEIGHT) && no_rock_left )
+			{
+				gameover_screen = try_again = 0;
+				starting = 1;
+			}
+		}
 	}	
 
 
 	//clean by destroying everything to free the system resources
 	SDL_DestroyTexture(tex_bg);
 	SDL_DestroyTexture(tex_player);
+	SDL_DestroyTexture(tex_rocks);
+	SDL_DestroyTexture(tex_gameover);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
